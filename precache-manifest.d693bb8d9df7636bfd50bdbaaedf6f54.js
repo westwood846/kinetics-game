f0a5a28d4d574cfd2075a7a50c321d79899488c4
turbopope <PapstDonB@Googlemail.com>
2019-08-06 19:03:19 +0800

    Updates

---
+++ precache-manifest.d693bb8d9df7636bfd50bdbaaedf6f54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "706600d306846a9d4349bfe15357d08e",
+    "url": "/kinetics-game/index.html"
+  },
+  {
+    "revision": "f29a0b3bbd28b0a2623d",
+    "url": "/kinetics-game/static/css/main.ce01f0ae.chunk.css"
+  },
+  {
+    "revision": "a0da9f55cbcfae3c613e",
+    "url": "/kinetics-game/static/js/2.ef3dd441.chunk.js"
+  },
+  {
+    "revision": "f29a0b3bbd28b0a2623d",
+    "url": "/kinetics-game/static/js/main.e3003c78.chunk.js"
+  },
+  {
+    "revision": "e12d332f0040428825c7",
+    "url": "/kinetics-game/static/js/runtime~main.577286e1.js"
+  }
+]);
