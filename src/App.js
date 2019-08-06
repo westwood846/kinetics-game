ccd7296089019e646589b4378eab2176e55df795
turbopope <PapstDonB@Googlemail.com>
2019-08-06 15:36:39 +0800

    Integrate kinetics into React app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import React from 'react';
 import './App.css';
+import { World as KineticsWorld, Body as KineticsBody, Vector } from './kinetics/kinetics';
+
+class Body extends React.Component {
+  render = () => <div className="Body" style={{top: this.props.body.position.y + "cm", left: this.props.body.position.x + "cm"}}>
+    <span>{this.props.body.position.toString()}</span><br/>
+    <span>{this.props.body.velocity.toString()}</span><br/>
+    <span>{this.props.body.mass}</span>
+  </div>
+}
+
+class World extends React.Component {
+  constructor(props) {
+    super(props);
+
+    let world = new KineticsWorld();
+    world.add(new KineticsBody(new Vector(2, 2), new Vector(0, 0), 6e12));
+    world.add(new KineticsBody(new Vector(2, 8), new Vector(4, 0), 1));
+
+    this.state = {world};
+
+    this.updateInterval = setInterval(this.update, 10);
+  }
+
+  update = () => {
+    let updatedWorld = {...this.state.world};
+    updatedWorld.update(1);
+    this.setState({world: updatedWorld});
+  }
+
+  render = () => {
+    console.log(this.state.world.bodies)
+    return (
+      <div className="World">
+        {this.state.world.bodies.map(body => <Body body={body}/>)}
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <World/>
     </div>
   );
 }
